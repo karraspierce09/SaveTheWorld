2d2f4d550b639961af4a6848213a487a48167751
Ricardo Blanco <ricardoblancobarbara@gmail.com>
2020-11-19 10:23:24 -0500

    Last commit before merge

---
+++ SaveTheWorld.Data/Migrations/202011191456201_FixingReply.cs
@@ -0,0 +1,16 @@
+namespace SaveTheWorld.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixingReply : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
