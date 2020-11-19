b748a8e9043cdc315e2a3d7cb9a62c07c5deaf6f
John Doe <harrison.jillian@gmail.com>
2020-11-19 18:05:27 -0500

    updated replies all now working in postman

--- SaveTheWorld.Services/ReplyService.cs
+++ SaveTheWorld.Services/ReplyService.cs
@@ -10,18 +10,18 @@ namespace SaveTheWorld.Services
     // These methods get called in the ReplyController
     // Add code, add references to .Services from .Data and .Models, add CRUD, add using statements
     public class ReplyService
-{
-
-    private readonly Guid _userId;  // 
-
-    public ReplyService(Guid userId)    // edited class name
     {
-        _userId = userId;
-    }
 
-    // Create: Create a reply
-    public bool CreateReply(ReplyCreate model)    // edited naming conventions
-    {
+        private readonly Guid _userId;  // 
+
+        public ReplyService(Guid userId)    // edited class name
+        {
+            _userId = userId;
+        }
+
+        // Create: Create a reply
+        public bool CreateReply(ReplyCreate model)    // edited naming conventions
+        {
             var entity =
                 new Reply()
                 {
@@ -32,93 +32,104 @@ namespace SaveTheWorld.Services
                     ModifiedUtc = null,
                     CreatedUtc = DateTime.Now
                     //Owner = model.Owner,
-                    
-            };
 
-        using (var ctx = new ApplicationDbContext())
-        {
-            ctx.Replies.Add(entity);
-            return ctx.SaveChanges() == 1;
-        }
-    }
-
-    // Read All: This method will allow us to see all the replies that belong to a specific user.
-    public IEnumerable<ReplyListItem> GetReplies()     // edited naming conventions
-    {
-        using (var ctx = new ApplicationDbContext())
-        {
-            var query =
-                ctx
-                    .Replies
-                    .Where(e => e.Id == _userId.ToString())
-                    .Select(
-                        e =>
-                            new ReplyListItem
-                            {
-                                ReplyId = e.ReplyId,
-                                ReplyText = e.ReplyText,
-                                CreatedUtc = e.CreatedUtc
-                            }
-                    );
-
-            return query.ToArray();
-        }
-    }
-
-    // Read Single: View a user's reply by its ReplyId. This is made after adding code to the ReplyController.cs
-    public ReplyDetail GetReplyById(int id)
-    {
-        using (var ctx = new ApplicationDbContext())
-        {
-            var entity =
-                ctx
-                    .Replies
-                    .Single(e => e.ReplyId == id);  // Change to this after adding the Owner class: .Single(e => e.ReplyId == id && e.OwnerId == _userId);
-            return
-                new ReplyDetail
-                {
-                    ReplyId = entity.ReplyId,
-                    ReplyText = entity.ReplyText,
-                       Tip = entity.Tip,
-                        Author = entity.Owner, // this was entity.author, changed to entity.owner
-                        CreatedUtc = entity.CreatedUtc,
-                    ModifiedUtc = entity.ModifiedUtc
                 };
-        }
-    }
 
-    // Update: Update/edit a reply
-    public bool UpdateReply(ReplyEdit model, int id)
-    {
-        using (var ctx = new ApplicationDbContext())
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Replies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        // Read All: This method will allow us to see all the replies that belong to a specific user.
+        public IEnumerable<ReplyListItem> GetReplies()     // edited naming conventions
         {
-            var entity =
-                ctx
-                    .Replies
-                    .Single(e => e.ReplyId == id);     //.Single(e => e.ReplyId == model.ReplyId);   // Once Owner is added: .Single(e => e.ReplyId == id && e.OwnerId == _userId); // OG: .Single(e => e.ReplyId == model.ReplyId && e.OwnerId == _userId);
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Replies
+                        .Where(e => e.Id == _userId.ToString())
+                        .Select(
+                            e =>
+                                new ReplyListItem
+                                {
+                                    ReplyId = e.ReplyId,
+                                    ReplyText = e.ReplyText,
+                                    TipId = e.TipId,
+                                //ModifiedUtc = null,
+                                CreatedUtc = DateTime.Now
 
-            entity.ReplyText = model.ReplyText;
-            entity.ModifiedUtc = DateTimeOffset.UtcNow;
+                                }
+                        );
 
-            return ctx.SaveChanges() == 1;
+                return query.ToArray();
+            }
         }
-    }
 
-    // Delete: Deleting a reply by its id
-    public bool DeleteReply(int replyId)
-    {
-        using (var ctx = new ApplicationDbContext())
+        // Read Single: View a user's reply by its ReplyId. This is made after adding code to the ReplyController.cs
+        public ReplyDetail GetReplyById(int id)
         {
-            var entity =
-                ctx
-                    .Replies
-                    .Single(e => e.ReplyId == replyId);  // .Single(e => e.ReplyId == replyId && e.OwnerId == _userId);
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Replies
+                        .Single(e => e.ReplyId == id);  // Change to this after adding the Owner class: .Single(e => e.ReplyId == id && e.OwnerId == _userId);
+                return
+                    new ReplyDetail
+                    {
+                        ReplyId = entity.ReplyId,
+                        ReplyText = entity.ReplyText,
+                        TipId = entity.TipId,
+                        //ModifiedUtc = null,
+                        CreatedUtc = DateTime.Now
 
-            ctx.Replies.Remove(entity);
-
-            return ctx.SaveChanges() == 1;
+                    };
+            }
         }
-    }
 
-}
+        // Update: Update/edit a reply
+        public bool UpdateReply(ReplyEdit model, int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Replies
+                        .Single(e => e.ReplyId == id);     //.Single(e => e.ReplyId == model.ReplyId);   // Once Owner is added: .Single(e => e.ReplyId == id && e.OwnerId == _userId); // OG: .Single(e => e.ReplyId == model.ReplyId && e.OwnerId == _userId);
+
+                entity.ReplyText = model.ReplyText;
+                entity.ModifiedUtc = DateTimeOffset.UtcNow;
+             //   entity.ReplyId = model.ReplyId;
+                entity.TipId = model.TipId;
+                    
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+
+
+
+
+
+// Delete: Deleting a reply by its id
+public bool DeleteReply(int replyId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Replies
+                       // .Single(e => e.ReplyId == replyId);  // .Single(e => e.ReplyId == replyId && e.OwnerId == _userId);
+                        .Single(e => e.ReplyId == replyId && e.Id == _userId.ToString());
+                ctx.Replies.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+    }
 }
